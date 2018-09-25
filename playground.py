f788691ba931d411567ef32b7f30126569449bf6
Jazilul Athoya <jazilulathoya@10-61-49-167.0200.bin.id.iosda.org>
2018-09-25 10:55:46 +0700

    change layout, add df shape

--- playground.py
+++ playground.py
@@ -6,18 +6,20 @@ import seaborn as sns
 df = pd.read_csv('googleplaystore.csv')
 
 #print(list(df))
-print(df.groupby(['Category']).size())
+print(df.shape[0])
+print(df[:1])
 
 cat = df.groupby(['Category']).size().to_frame(name = 'count').reset_index()
 cat_sorted = cat.sort_values(by='count', ascending=False)
 
 print(cat_sorted)
-sns.set(style="whitegrid")
+#sns.set(style="whitegrid")
+sns.set_color_codes("muted")
 
 # vertical bar plot
 #ax = sns.barplot(x="Category", y="count", data=cat_sorted[:10])
 #ax.set_xticklabels(ax.get_xticklabels(),rotation=90)
 
 # horizontal bar plot
-ax = sns.barplot(x="count", y="Category", data=cat_sorted[:10])
+ax = sns.barplot(x="count", y="Category", data=cat_sorted[:10], color='b')
 #ax.set_xticklabels(ax.get_xticklabels(),rotation=90)
